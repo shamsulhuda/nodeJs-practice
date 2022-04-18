df7b57f6dcc21d9a68f8b251bd5c604990f21b47
shamsulhuda <shamsulhuda310@gmail.com>
2022-04-18 15:52:10 +0600

    http module

--- index.js
+++
@@ -1,8 +0,0 @@
-const School = require('./school');
-
-// register a listener for event
-const school = new School();
-school.on('bellRing', ({ period, text }) => {
-    console.log(`The class is over becouse, ${period} ${text}`);
-});
-school.startPeriod();
